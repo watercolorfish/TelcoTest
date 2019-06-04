d4cd02d8b7b71d63dfb49a5a5b8dac65844a0d5a
Mozol Daria <orliet@yandex.ru>
2019-06-04 14:29:59 +0300

    Trying to do something with SQL

--- source.py
+++ source.py
@@ -1,5 +1,6 @@
 import datetime
 import sqlite3
+#port MySQLdb
 import os
 import paramiko
 
@@ -9,7 +10,6 @@ if __name__ == "__main__":
     for line in file:
         if (line.startswith('sftp_host')):
             host = (line.split('='))[1].split('\n')[0]
-            print(host)
         if (line.startswith('sftp_port')):
             port = (line.split('='))[1].split('\n')[0]
         if (line.startswith('sftp_user')):
@@ -20,29 +20,32 @@ if __name__ == "__main__":
             directory = (line.split('='))[1].split('\n')[0]
         if (line.startswith('local_dir')):
             local_dir = (line.split('='))[1].split('\n')[0]
-            print(local_dir)
-        #arr.append(line)
+        if (line.startswith('sql_user')):
+            sql_user = (line.split('='))[1].split('\n')[0]
+        if (line.startswith('sql_password')):
+            sql_password = (line.split('='))[1].split('\n')[0]
+        if (line.startswith('sql_database')):
+            sql_database = (line.split('='))[1].split('\n')[0]
+
     file.close()
 
     files = os.listdir(directory)
 
-    conn = sqlite3.connect("database.db")
+    conn = MySQLdb.connect(user=sql_user, passwd=sql_password, db=sql_database)
     cursor = conn.cursor()
-    cursor.execute("""CREATE TABLE information
+    cursor.execute("""CREATE TABLE IF NOT EXIST information
                       (date and time, name of file)
                    """)
-
+    conn.commit()
     transport = paramiko.Transport((host, port))
     transport.connect(username=user, password=password)
     sftp = paramiko.SFTPClient.from_transport(transport)
 
     for remote_dir in files:
-        remotepath = remote_dir
-
-        sftp.get(remotepath, local_dir)
-        sftp.put(local_dir, remotepath)
+        sftp.get(remote_dir, local_dir)
         now = datetime.datetime.now()
         now.strftime("%d-%m-%Y %H:%M")
+        cursor.executemany("INSERT INTO albums VALUES (?,?)", (now, remote_dir))
 
     sftp.close()
     transport.close()
